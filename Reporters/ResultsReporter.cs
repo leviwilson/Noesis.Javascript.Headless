cdaaf8c212ffd20f370ba134122a37107f47084d
Levi Wilson <levi@leviwilson.com>
2011-11-07 09:01:16 -0500

    modified to report each individual Jasmine test as a test in NUnit

---
+++ Reporters/ResultsReporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Noesis.Javascript.Headless.Reporters
+{
+    public class ResultsReporter : IJavaScriptReporter
+    {
+        private readonly Dictionary<string, string> _results = new Dictionary<string, string>();
+        public Dictionary<string, string> Results
+        {
+            get { return _results; }
+        }
+
+        public string Result
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Passed(string name)
+        {
+            _results.Add(name, null);
+        }
+
+        public void Failed(string name, object[] errors)
+        {
+            var scriptError = errors.Cast<string>()
+                .Aggregate((error, nextError) => error + Environment.NewLine + nextError);
+            _results.Add(name, scriptError);
+        }
+
+        public void Finished()
+        {
+        }
+    }
+}
